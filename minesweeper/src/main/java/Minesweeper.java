688b3f96cfae2e6ccf3ffa5738c4dc07b2da33aa
Marcus Michaud <mmichaud.pro@gmail.com>
2016-03-17 12:46:10 +0100

    minesweeper base logic

--- minesweeper/src/main/java/Minesweeper.java
+++ minesweeper/src/main/java/Minesweeper.java
@@ -25,22 +25,134 @@ public class Minesweeper {
 		this.boundMaxM = boundMaxM;
 	}
 	
-	
-	private int getValueWhenNEqual0(int m, int n) {
-		adjacentValue = 0;
+	public int getAdjacentValue(int m, int n) {
 		
+		if (m == boundMinM) {
+			if(n == boundMinN) {
+				this.getValueWhenBoundMinMAndBoundMinN(m, n);
+			}
+			if ((n > boundMinN) && (n < boundMaxN)) {
+				this.getValueWhenBoundMinMAndNotBoundN(m, n);
+			}
+			if (n == boundMaxN) {
+				this.getValueWhenBoundMinMAndBoundMaxN(m, n);
+			}
+		} else if ((boundMinM < m) && (m < boundMaxM)) {
+			
+			if(n == boundMinN) {
+				this.getValueWhenNotBoundMAndBoundMinN(m,n);
+			}
+			if ((n > boundMinN) && (n < boundMaxN)) {
+				this.getValueWhenNotBoundMAndNotBoundN(m,n);
+			}
+			if (n == boundMaxN) {
+				this.getValueWhenNotBoundMAndBoundMaxN(m,n);
+			}
+		} else if (boundMaxM == m) {
+			if(n == boundMinN) {
+				this.getValueWhenBoundMaxMAndBoundMinN(m,n);
+			}
+			if ((n > boundMinN) && (n < boundMaxN)) {
+				this.getValueWhenBoundMaxMAndNotBoundN(m,n);
+			}
+			if (n == boundMaxN) {
+				this.getValueWhenBoundMaxMAndBoundMaxN(m,n);
+			}
+		}
+		return adjacentValue;
+	}
+	
+	private int getValueWhenBoundMaxMAndBoundMaxN(int m, int n) {
+		adjacentValue = 0;
+		if (inputGround[m-1][n-1] == STAR)
+			adjacentValue++;
+		if (inputGround[m-1][n] == STAR)
+			adjacentValue++;
+		if (inputGround[m][n-1] == STAR)
+			adjacentValue++;
+		return adjacentValue;
+	}
+	
+	private int getValueWhenBoundMaxMAndNotBoundN(int m, int n) {
+		adjacentValue = 0;
+		if (inputGround[m-1][n-1] == STAR)
+			adjacentValue++;
+		if (inputGround[m-1][n] == STAR)
+			adjacentValue++;
+		if (inputGround[m-1][n+1] == STAR)
+			adjacentValue++;
+		if (inputGround[m][n-1] == STAR)
+			adjacentValue++;
+		if (inputGround[m][n+1] == STAR)
+			adjacentValue++;
+		return adjacentValue;
+	}
+
+	private int getValueWhenBoundMaxMAndBoundMinN(int m, int n) {
+		adjacentValue = 0;
+		if (inputGround[m-1][n] == STAR)
+			adjacentValue++;
+		if (inputGround[m-1][n+1] == STAR)
+			adjacentValue++;
+		if (inputGround[m][n+1] == STAR)
+			adjacentValue++;
+		return adjacentValue;
+		
+	}
+
+	private int getValueWhenNotBoundMAndBoundMaxN(int m, int n) {
+
+		adjacentValue = 0;
+		if (inputGround[m-1][n-1] == STAR)
+			adjacentValue++;
+		if (inputGround[m-1][n] == STAR)
+			adjacentValue++;
+		if (inputGround[m][n-1] == STAR)
+			adjacentValue++;
+		if (inputGround[m+1][n-1] == STAR)
+			adjacentValue++;
+		if (inputGround[m+1][n] == STAR)
+			adjacentValue++;
+		
+		return adjacentValue;
+		
+	}
+
+	private int getValueWhenNotBoundMAndNotBoundN(int m, int n) {
+		adjacentValue = 0;
+		if (inputGround[m-1][n-1] == STAR)
+			adjacentValue++;
+		if (inputGround[m-1][n] == STAR)
+			adjacentValue++;
+		if (inputGround[m-1][n+1] == STAR)
+			adjacentValue++;
+		if (inputGround[m][n-1] == STAR)
+			adjacentValue++;
+		if (inputGround[m][n+1] == STAR)
+			adjacentValue++;
+		if (inputGround[m+1][n-1] == STAR)
+			adjacentValue++;
 		if (inputGround[m+1][n] == STAR)
 			adjacentValue++;
 		if (inputGround[m+1][n+1] == STAR)
 			adjacentValue++;
-		if (inputGround[m][n+1] == STAR)
-			adjacentValue++;
 		
+		return adjacentValue;
+		
+	}
+
+	private int getValueWhenBoundMinMAndBoundMinN(int m, int n) {
+		adjacentValue = 0;
+		
+		if ((inputGround[m+1][n] == STAR)
+			|| (inputGround[m+1][n+1] == STAR)
+			|| (inputGround[m][n+1] == STAR)) {
+			adjacentValue++;
+		}
 		return adjacentValue;
 	}
 	
-	
-	private int getValueWhenNGreaterThan0(int m, int n) {
+	private int getValueWhenBoundMinMAndNotBoundN(int m, int n) {
 		adjacentValue = 0;
 		
 		if (inputGround[m][n-1] == STAR)
@@ -57,7 +169,7 @@ public class Minesweeper {
 		return adjacentValue;
 	}
 	
-	private int getValueWhenNEqualBoundN(int m, int n) {
+	private int getValueWhenBoundMinMAndBoundMaxN(int m, int n) {
 		adjacentValue = 0;
 
 		if (inputGround[m][n-1] == STAR)
@@ -69,22 +181,18 @@ public class Minesweeper {
 		
 		return adjacentValue;
 	}
-	
-	public int getAdjacentValue(int m, int n) {
-		
-		if (inputGround[m][n] != STAR) {
-			if (m == boundMinM) {
-				if(n == 0) {
-					this.getValueWhenNEqual0(m, n);
-				}
-				if ((n > 0) && (n < boundMaxN)) {
-					this.getValueWhenNGreaterThan0(m, n);
-				}
-				if (n == boundMaxN) {
-					this.getValueWhenNEqualBoundN(m, n);
-				}
-			}
-		}
+	private int getValueWhenNotBoundMAndBoundMinN(int m, int n) {
+		adjacentValue = 0;
+		if (inputGround[m-1][n] == STAR)
+			adjacentValue++;
+		if (inputGround[m-1][n+1] == STAR)
+			adjacentValue++;
+		if (inputGround[m][n+1] == STAR)
+			adjacentValue++;
+		if (inputGround[m+1][n] == STAR)
+			adjacentValue++;
+		if (inputGround[m+1][n+1] == STAR)
+			adjacentValue++;
 		return adjacentValue;
 	}
 }
